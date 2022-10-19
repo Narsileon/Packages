0fcd39fb9edb671e83ed8a08a6c25079a5417244
Narsilien <rigaux.jonathan@gmail.com>
2022-10-19 08:48:00 +0200

    Update

--- resources/js/Shared/Layouts/Web/Layout.jsx
+++ resources/js/Shared/Layouts/Web/Layout.jsx
@@ -1,10 +1,10 @@
-import { usePage } from "@inertiajs/inertia-react";
-import Header from "./Header";
-import Footer from "./Footer";
+import { Link } from "@inertiajs/inertia-react";
+import Nav from "./Nav";
+import LocaleDropdown from "@/Shared/Layouts/LocaleDropdown";
+import ThemeButton from "@/Shared/Layouts/ThemeButton";
+import WebsiteLogo from "@/Shared/Layouts/WebsiteLogo";
 
 export default function Layout({ children }) {
-    const auth = usePage().props.auth;
-
     return (
         <main className="flex flex-col min-h-screen">
             <Header />
@@ -16,4 +16,42 @@ export default function Layout({ children }) {
             <Footer />
         </main>
     );
+}
+
+const Header = () => {
+    return (
+        <header className="primary-background border-b-2 border-gray-500 flex justify-between p-2">
+            <div className="flex items-center">
+                <WebsiteLogo />
+            </div>
+
+            <div className="flex items-center space-x-2">
+                <LocaleDropdown />
+                <Nav />
+                <ThemeButton />                   
+            </div>
+        </header>        
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer className="primary-background w-full p-2 border-t-2 border-gray-500 text-sm">
+            <div className="flex items-center justify-between p-2">
+                <WebsiteLogo />
+                <ul className="flex flex-wrap items-center space-x-4">
+                    {/* Links (Texts) */}
+                </ul>
+            </div>
+            <hr className="my-2 border-gray-500" />
+            <div className="flex items-center justify-between p-2">
+                <span>
+                    © 2022 <Link href={route('home')} className="hover:underline">Narsil Studio™</Link>. All Rights Reserved.
+                </span>
+                <div className="flex space-x-4 h-6">
+                    {/* Links (Icons) */}
+                </div>
+            </div>
+        </footer>         
+    ) 
 }
