b64f33723ca09332d37a70ecbe33ae263107a6a4
Narsilien <rigaux.jonathan@gmail.com>
2022-10-19 06:56:17 +0200

    Update

--- resources/js/Pages/Backoffice/Calendar/Index.jsx
+++ resources/js/Pages/Backoffice/Calendar/Index.jsx
@@ -1,8 +1,12 @@
+import { Head } from "@inertiajs/inertia-react";
+import { t } from "@/localization";
 import Calendar from "@/Components/Calendars/Calendar";
 
 export default function Index() {
     return (
         <>
+            <Head title={ t("Calendar") } />
+
 			<Calendar />        
         </>
     );
