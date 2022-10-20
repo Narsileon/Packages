9bd16da74b51c5ee0860c4f6b213b093b6e17085
Narsilien <rigaux.jonathan@gmail.com>
2022-10-20 07:22:06 +0200

    Update

--- resources/js/localization.js
+++ resources/js/localization.js
@@ -1,15 +1,13 @@
 const t = (key, replacements = null) => {
     let text;
 
-    if (!window._localization)
-    {
+    if (!window._localization) {
         text = key;
     } else {
         text = window._localization[key] || key;
     }
 
-    if (replacements)
-    {
+    if (replacements) {
         Object.keys(replacements).forEach(x => {
             text = localization.replace(`:${x}`, replacements[r]);
         })
