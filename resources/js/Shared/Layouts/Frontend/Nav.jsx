c6c85ce01c1ffa9b00e59fc79ec7b61deaffb6ab
Narsilien <rigaux.jonathan@gmail.com>
2022-11-15 08:23:29 +0100

    Update

--- resources/js/Shared/Layouts/Frontend/Nav.jsx
+++ resources/js/Shared/Layouts/Frontend/Nav.jsx
@@ -1,18 +1,27 @@
-import { useToggle } from "react-use";
+import { useClickAway, useToggle } from "react-use";
 import { usePage } from "@inertiajs/inertia-react";
 import { trans } from "@/narsil-localization";
+import CloseButton from "@/Components/Elements/Buttons/CloseButton";
 import MenuButton from "@/Components/Elements/Buttons/MenuButton";
 import NavLink from "@/Components/Elements/Links/NavLink";
+import { useRef } from "react";
 
 export default function Nav() {
     const auth = usePage().props.auth;
 
-    const commonLinks = [
+    const links = [
         { route: route('home'), label: trans('common.home'), icon: 'home' },
     ];
 
+    const dropdown = useRef();
+
     const [open, setOpen] = useToggle(false);
-    const [open2, setOpen2] = useToggle(false);
+
+    useClickAway(dropdown, function() {
+        if (open) {
+            setOpen(false)
+        }
+    }, ['mouseup']);
 
     return (
         <nav className="flex items-center font-semibold">
@@ -21,16 +30,24 @@ export default function Nav() {
                 onClick={ setOpen }
             />
 
-            <ul className={ `md:flex md:space-x-4 ${ open ? "absolute primary-background w-full h-screen top-0 left-0 z-40" : "hidden" }` }>
-                { renderLinks(commonLinks) }
+            <ul className={ `md:flex md:justify-between primary-background p-2 space-y-2 md:space-x-4 ${ open ? "absolute md:relative md:w-auto w-full md:h-12 h-screen top-0 left-0 z-40" : "hidden" }` }>
+                { renderLinks(links) }
 
-                <li className="relative grid">
+                <li className="md:relative grid">
                     <MenuButton
                         className="hidden md:block"
-                        onClick={ setOpen2 }
+                        onClick={ setOpen }
                     />
 
-                    <ul className={ `${ !open && open2 ? "absolute primary-background border-2 border-color p-2 space-y-2 top-12 right-0 z-40 rounded" : "md:hidden" }` }>
+                    <CloseButton
+                        className="absolute top-4 right-4 w-6 h-6 md:hidden"
+                        onClick={ () => setOpen(false) }
+                    />
+
+                    <ul
+                        className={ `${ open ? "md:absolute relative md:primary-background md:border-2 md:p-2 md:border-color md:top-8 md:right-0 space-y-2 z-40 rounded" : "md:hidden" }` }
+                        ref={ dropdown }
+                    >
                         { auth ? <NavAuth /> : <NavGuest /> }
                     </ul>
                 </li>
